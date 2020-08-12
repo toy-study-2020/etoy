a72593aa7cbcf65057094158e8fcb37e2e8f9079
tnwjd2050 <tnwjd2050@naver.com>
2020-08-12 23:46:09 +0900

    초기 셋팅

--- 박수정/search.js
+++ 박수정/search.js
@@ -1,1 +1,25 @@
 // Search TODO
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+function handleSubmit(e) {
+    e.preventDefault()
+    searchInput.value = '';
+}
+
+function handleInput() {
+console.log('input')
+}
+
+function loadToKeywords() {
+    console.log(keyword)
+}
+
+function init() {
+    loadToKeywords();
+    searchForm.addEventListener('submit', handleSubmit)
+    searchInput.addEventListener('input', handleInput)
+
+}
+
+init();
