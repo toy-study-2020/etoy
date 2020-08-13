81ba2f246d580ad814da592d1c2cd6538207af62
baekCode <sosaer88@gmail.com>
2020-08-13 11:00:14 +0900

    feat : styled theme 적용

--- 백주은/client/src/index.tsx
+++ 백주은/client/src/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root'),
+);
