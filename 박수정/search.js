4910e4dfbcd8f81dfc49471039a0ae5c39597223
tnwjd2050 <tnwjd2050@naver.com>
2020-08-13 21:43:53 +0900

    검색 텍스트 강조 기능 추가

--- 박수정/search.js
+++ 박수정/search.js
@@ -2,15 +2,26 @@
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const recommendKeywordWrap = document.querySelector('.recommend-keyword');
+let emphasisedItem = '';
 
 function handleSubmit(e) {
     e.preventDefault()
     searchInput.value = '';
-}
+};
 
 function resetKeywords() {
     recommendKeywordWrap.innerHTML = '';
-}
+};
+
+function emphasisingItem(arr, emphasisString) {
+    arr.forEach((splitedItem, i) => {
+        emphasisedItem = arr[0];
+
+        if (i > 0) {
+            emphasisedItem += `<em>${emphasisString}</em>${splitedItem}`;
+        }
+    });
+};
 
 function loadToKeywords(enteredData) {
     const filteredKeyword = keyword.filter(data => {
@@ -18,20 +29,23 @@ function loadToKeywords(enteredData) {
     });
 
     filteredKeyword.forEach(function(item) {
-        recommendKeywordWrap.insertAdjacentHTML('beforeend', `<li><a href="#">${item}</a></li>`);
+        const splitItem = item.split(enteredData);
+
+        emphasisingItem(splitItem, enteredData);
+        recommendKeywordWrap.insertAdjacentHTML('beforeend', `<li><a href="#">${emphasisedItem}</a></li>`);
     });
-}
+};
 
 function handleInput() {
     const value = this.value;
 
     resetKeywords();
     if (value.length) loadToKeywords(value);
-}
+};
 
 function init() {
-    searchForm.addEventListener('submit', handleSubmit)
-    searchInput.addEventListener('input', handleInput)
-}
+    searchForm.addEventListener('submit', handleSubmit);
+    searchInput.addEventListener('input', handleInput);
+};
 
 init();
