b3265f7c347e78c99c89304ffbf3d3fd332ac730
baekCode <sosaer88@gmail.com>
2020-08-13 18:32:22 +0900

    feat : SearchPresenter 구현

--- 백주은/client/src/Components/Search/SearchPresenter.tsx
+++ 백주은/client/src/Components/Search/SearchPresenter.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import styled from '../../styles/typed-components';
 
-const SearchPresenter = () => {
-  return <div>검색</div>;
+interface IProps {
+  onSubmit: any;
+  onChange: any;
+  keyword: { keyword?: string[]; matchKeyword?: string[] };
+}
+
+const Container = styled.form``;
+const Input = styled.input``;
+const Button = styled.button``;
+
+const SearchPresenter: React.SFC<IProps> = ({ onSubmit, onChange, keyword }) => {
+  return (
+    <Container onSubmit={onSubmit}>
+      <Input onChange={onChange} />
+      <Button>검색</Button>
+      {keyword.matchKeyword && keyword.matchKeyword.map((item: string, index: number) => <div key={index}>{item}</div>)}
+    </Container>
+  );
 };
 
 export default SearchPresenter;
