66a3094025e16f69b00a6590abad5f1ab025769a
minjeongkang <snowdrop1202@gmail.com>
2020-08-15 03:32:33 +0900

    Search 생성자 함수 생성 및 변수 초기화

--- 강민정/search.js
+++ 강민정/search.js
@@ -1,1 +1,33 @@
-console.log('mj kang');
+const search = (function() {
+  'use strict';
+  const Search = function(args) {
+    if (!args) {
+      return;
+    }
+
+    const defaults = {
+      form: null,
+      input: 'input[type="search"]',
+      buttonSubmit: 'button[type="submit"]',
+      resultWrap: null,
+      result: null
+    }
+
+    this.wrapper = document.querySelector(args.wrapper ? args.wrapper : args);
+    this.form = this.wrapper.querySelector('form');
+    this.input = this.wrapper.querySelector(args.input ? args.input : defaults.input);
+    this.buttonSubmit = this.wrapper.querySelector(args.buttonSubmit ? args.buttonSubmit : defaults.buttonSubmit);
+    this.resultWrap = this.wrapper.querySelector(args.resultWrap ? args.resultWrap : defaults.resultWrap);
+    this.result = this.resultWrap.querySelector(args.result ? args.result : defaults.result);
+    this.resultEl = 'li';
+    this.list = null;
+    this.listArray = [];
+    this.listLimit = 10;
+    this.data = args.data ? args.data : null;
+    this.lengthData = this.data.length;
+    this.isOpened = false;
+    this.keyword = null;
+    this.wordList = null;
+    this.wordFirstIndex = 0;
+    this.toggleClass = 'active';
+  };
