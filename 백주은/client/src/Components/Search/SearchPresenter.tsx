4a4ee36c32fa24d7ab457ae7ad0f63178048c1ef
baekCode <sosaer88@gmail.com>
2020-08-14 22:30:11 +0900

    feat : 인기검색어 스타일 구현

--- 백주은/client/src/Components/Search/SearchPresenter.tsx
+++ 백주은/client/src/Components/Search/SearchPresenter.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import { Container, SaerchForm, SearchList, SearchResult, SearchItem, Form, Input, Button } from './SearchStyles';
+import { Tabs } from 'react-tabs';
+import {
+  Container,
+  SaerchForm,
+  SearchList,
+  SearchResult,
+  SearchItem,
+  Form,
+  Input,
+  Button,
+  TabNav,
+  TabNavItem,
+  TabContent,
+} from './SearchStyles';
+
+const INPUT_PLACEHOLDER = '영어로 검색하세요.';
 
 interface IProps {
   onSubmit: any;
@@ -11,18 +24,40 @@ interface IProps {
   focus: boolean;
   inputFocus: boolean;
   childRef: React.RefObject<HTMLDivElement>;
-  keyword: { keyword?: string[]; matchKeyword?: string[] };
+  keyword: { keyword?: string[]; matchKeyword?: string[]; bestKeyword?: string[] };
 }
 
-const SearchResultArea = () => {
+interface IPropsResult {
+  bestKeyword: any;
+}
+
+const SearchResultArea: React.SFC<IPropsResult> = ({ bestKeyword }) => {
+  const bestfive = bestKeyword.slice(0, 5);
+  const rest = bestKeyword.slice(5, 10);
   return (
     <Tabs>
-      <TabList>
-        <Tab>인기검색어</Tab>
-        <Tab>최근검색어</Tab>
-      </TabList>
-      <TabPanel>인기검색어 출력</TabPanel>
-      <TabPanel>최근검색어 출력</TabPanel>
+      <TabNav>
+        <TabNavItem>인기검색어</TabNavItem>
+        <TabNavItem>최근검색어</TabNavItem>
+      </TabNav>
+      <TabContent>
+        <div>
+          {bestfive.map((item: string, index: number) => (
+            <div key={index}>
+              <span>{index + 1}</span> {item}
+            </div>
+          ))}
+        </div>
+        <div>
+          {rest.map((item: string, index: number) => (
+            <div key={index}>
+              <span>{index + 6}</span>
+              {item}
+            </div>
+          ))}
+        </div>
+      </TabContent>
+      <TabContent>최근검색어 출력</TabContent>
     </Tabs>
   );
 };
@@ -32,7 +67,7 @@ const SearchPresenter: React.SFC<IProps> = ({ onSubmit, onChange, onFocus, focus
     <Container ref={childRef}>
       <Form onSubmit={onSubmit}>
         <SaerchForm>
-          <Input onChange={onChange} onClick={onFocus} />
+          <Input onChange={onChange} onClick={onFocus} placeholder={INPUT_PLACEHOLDER} />
           <Button>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#ffffff">
               <path d="M23.809 21.646l-6.205-6.205c1.167-1.605 1.857-3.579 1.857-5.711 0-5.365-4.365-9.73-9.731-9.73-5.365 0-9.73 4.365-9.73 9.73 0 5.366 4.365 9.73 9.73 9.73 2.034 0 3.923-.627 5.487-1.698l6.238 6.238 2.354-2.354zm-20.955-11.916c0-3.792 3.085-6.877 6.877-6.877s6.877 3.085 6.877 6.877-3.085 6.877-6.877 6.877c-3.793 0-6.877-3.085-6.877-6.877z" />
@@ -42,7 +77,7 @@ const SearchPresenter: React.SFC<IProps> = ({ onSubmit, onChange, onFocus, focus
       </Form>
       {inputFocus && (
         <SearchList>
-          {focus && <SearchResultArea />}
+          {focus && <SearchResultArea bestKeyword={keyword.bestKeyword} />}
           {keyword.matchKeyword && (
             <SearchResult>
               {keyword.matchKeyword.map((item: string, index: number) => (
--- 백주은/client/src/Components/Search/SearchStyles.tsx
+++ 백주은/client/src/Components/Search/SearchStyles.tsx
@@ -1,24 +1,30 @@
 import styled from '../../styles/typed-components';
+import { Tab, TabList, TabPanel } from 'react-tabs';
 
 export const Container = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  margin: 50px 0;
+  width: 600px;
+  margin: 50px auto;
+  border: 3px solid ${props => props.theme.blueColor};
 `;
 export const SaerchForm = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 600px;
-  border: 3px solid ${props => props.theme.blueColor};
+  width: 100%;
 `;
-export const Form = styled.form``;
+export const Form = styled.form`
+  width: 100%;
+`;
 export const Input = styled.input`
   width: 100%;
   height: 40px;
   padding: 0 20px;
+  border: none;
   outline: none;
 `;
 export const Button = styled.button`
@@ -29,4 +35,68 @@ export const Button = styled.button`
   cursor: pointer;
   outline: none;
 `;
-export const SearchList = styled.div``;
+export const SearchList = styled.div`
+  overflow-y: auto;
+  position: absolute;
+  left: -3;
+  top: 43px;
+  width: calc(100% + 6px);
+  height: 200px;
+  padding: 15px 10px;
+  border: 1px solid #eee;
+`;
+export const SearchResult = styled.div``;
+export const SearchItem = styled.div`
+  & + & {
+    margin-top: 10px;
+  }
+`;
+
+export const TabNav = styled(TabList)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 10px;
+  border-bottom: 1px solid #eee;
+`;
+
+export const TabNavItem = styled(Tab)`
+  position: relative;
+  width: 50%;
+  padding: 5px 0;
+  text-align: center;
+  cursor: pointer;
+  &.react-tabs__tab--selected::before {
+    content: '';
+    position: absolute;
+    width: 50%;
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    transform: translate(50%, 100%);
+    background: ${props => props.theme.blueColor};
+  }
+`;
+export const TabContent = styled(TabPanel)`
+  display: flex;
+  justify-content: space-between;
+  > div {
+    width: 50%;
+    > div {
+      display: flex;
+      padding: 5px;
+      span {
+        width: 18px;
+        height: 18px;
+        margin-right: 5px;
+        font-size: 12px;
+        color: #fff;
+        text-align: center;
+        line-height: 20px;
+        border-radius: 9px;
+        background: ${props => props.theme.blueColor};
+      }
+    }
+  }
+`;
