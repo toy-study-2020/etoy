c61397a63039047f24926e6f6eb5af7445d519e0
baekCode <sosaer88@gmail.com>
2020-08-15 09:34:40 +0900

    chore : eslint-disabled-next-line설정으로 다음코드 무시

--- 백주은/client/src/Components/Search/SearchContainer.tsx
+++ 백주은/client/src/Components/Search/SearchContainer.tsx
@@ -31,7 +31,7 @@ const SearchContainer = () => {
         setFocus(false);
         setInputFocus(false);
       }
-    });
+    }); // eslint-disable-next-line
   }, [inputFocus]);
 
   const getKeyword = async () => {
