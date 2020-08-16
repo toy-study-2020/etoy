2e077bf928486ed0090f15e2ae82f0035d2d1f02
songcs <songcs0329@gmail.com>
2020-08-16 14:59:18 +0900

    200816 주은님 PR사항 수정

--- 송창석/search.js
+++ 송창석/search.js
@@ -1,22 +1,10 @@
 // Search TODO
 
 class formSearch {
-    constructor (selector, maxLength) {
-        this.selector = selector;
+    constructor (element, maxLength) {
+        this.element = element;
+        this.setEvent(this.element);
         this.maxLength = maxLength;
-    }
-    setInit() {
-        const searchEl = {
-            wrapper: document.querySelector(this.selector),
-            form: document.querySelector("#search > form"),
-            input: document.querySelector("#search-input"),
-            close: document.querySelector(".close button"),
-            list: document.querySelector(".related_list"),
-            maxLength: this.maxLength,
-            items: null,
-            focusIndex: null
-        }
-        this.setEvent(searchEl);
     }
     setEvent(searchEl) {
         searchEl.form.addEventListener('submit', (event) => {
@@ -29,46 +17,52 @@ class formSearch {
             if(event.keyCode === 40 || event.keyCode === 38) {
                 if (event.keyCode === 40) searchEl.focusIndex = searchEl.focusIndex >= searchEl.items.length - 1 ? 0 : searchEl.focusIndex + 1;
                 if (event.keyCode === 38) searchEl.focusIndex = searchEl.focusIndex <= 0 ? searchEl.items.length - 1 : searchEl.focusIndex - 1;
-                focusList(searchEl.focusIndex, searchEl.items, searchEl.input);
+                this.focusList(searchEl.focusIndex, searchEl.items, searchEl.input);
             } else if(event.keyCode != 13) {
-                const components = keyFilter(event.target.value, searchEl.maxLength);
-                renderItems(searchEl, components);
+                const components = this.keyFilter(event.target.value, this.maxLength);
+                this.renderItems(searchEl, components);
                 searchEl.focusIndex = searchEl.items.length;
             }
 
-            if(event.target.value) showRelateList(searchEl.wrapper);
-            if(event.target.value === '') hideRelateList(searchEl.wrapper);
+            if(event.target.value) this.showRelateList(searchEl.wrapper);
+            if(event.target.value === '') this.hideRelateList(searchEl.wrapper);
         });
 
-        searchEl.close.addEventListener('click', () => hideRelateList(searchEl.wrapper));
+        searchEl.close.addEventListener('click', () => this.hideRelateList(searchEl.wrapper));
+    }
+    keyFilter(value, maxLength) {
+        const filtered = keyword.filter(word => {
+            if(word.substring(0, value.length).indexOf(value) > -1) return word
+        }).slice(0, maxLength);
+        
+        const render = filtered.reduce((acc, value, index) => {
+            return acc += `<li data-idx=${index}><a href="#n">${value}</a></li>`
+        },"")
+        
+        return render;
+    }
+    renderItems(el, components) {
+        el.list.innerHTML = components;
+        el.items = el.list.querySelectorAll("a");
+    }
+    focusList(idx, items, input) {
+        items.forEach(item => item.classList.remove('focused'));
+        items[idx].classList.add('focused');
+        input.value = items[idx].innerText;
+    }
+    showRelateList(wrapper) {
+        wrapper.classList.add("has_value");
+    }
+    hideRelateList(wrapper) {
+        wrapper.classList.remove("has_value");
     }
 }
 
-const keyFilter = (value, maxLength) => {
-    const filtered = keyword.filter(word => {
-        if(word.substring(0, value.length).indexOf(value) > -1) return word
-    }).slice(0, maxLength);
-    
-    let renderGroup = [];
-    for(let i = 0; i < filtered.length; i++) renderGroup += `<li data-idx="${i}"><a href="#n">${filtered[i]}</a></li>`;
-    return renderGroup;
+const element = {
+    wrapper: document.querySelector("#search"),
+    form: document.querySelector("#search > form"),
+    input: document.querySelector("#search-input"),
+    close: document.querySelector(".close button"),
+    list: document.querySelector(".related_list"),
 }
-
-const renderItems = (el, components) => {
-    el.list.innerHTML = components;
-    el.items = el.list.querySelectorAll("a");
-}
-
-const focusList = (idx, items, input) => {
-    items.forEach(item => item.classList.remove('focused'));
-    items[idx].classList.add('focused');
-    input.value = items[idx].innerText;
-}
-
-const showRelateList = (wrapper) => wrapper.classList.add("has_value");
-const hideRelateList = (wrapper) => wrapper.classList.remove("has_value");
-
-
-
-const search = new formSearch('#search', 10);
-search.setInit();
+const search = new formSearch(element, 10);
