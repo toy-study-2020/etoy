e9f4ba554d3ce20e1ee85b6de210c081d72837ac
minjeongkang <snowdrop1202@gmail.com>
2020-08-19 09:55:08 +0900

    검색 자동 완성 기능 추가
    - 검색어 선택 시 해당 검색어 검색 추가
    - 입력 검색어 `em` 태그로 강조
    - 기존 prototype 함수 분리

--- 강민정/search.js
+++ 강민정/search.js
@@ -31,12 +31,14 @@ const search = (function() {
     this.wordList = null;
     this.FIRST_INDEX = defaults.FIRST_INDEX;
     this.toggleClass = 'active';
+    this.inputValue = null;
+    this.keywordLength = null;
+    this.keywordHTML = null;
 
     this.shuffle(this.data);
 
     this.input.addEventListener('keyup', this.open.bind(this));
-    this.input.addEventListener('focus', this.open.bind(this));
-    this.form.addEventListener('submit', this.submit.bind(this));
+    this.form.addEventListener('submit', this.submit.bind(this, this.input.value));
   };
 
   Search.prototype.shuffle = function(data) {
@@ -87,6 +89,7 @@ const search = (function() {
     this.isOpened = false;
     this.resultWrap.classList.remove(this.toggleClass);
     window.removeEventListener('click', this.handlerSearch);
+    window.removeEventListener('click', this.eventResult);
   };
 
   Search.prototype.createElement = function(el) {
@@ -95,7 +98,9 @@ const search = (function() {
 
   Search.prototype.appendResult = function(wrapper, direction, el, data) {
     if (data) {
-      el.innerText = data;
+      this.keywordLength = this.keyword.length;
+      this.keywordHTML = data.replace(this.keyword, `<em>` + this.keyword + `</em>`);
+      el.innerHTML = `<a href="#">` + this.keywordHTML + `</a>`;
     }
     this.result.insertAdjacentElement(direction, el);
   };
@@ -108,36 +113,44 @@ const search = (function() {
 
   Search.prototype.recommend = function() {
     this.resultInit();
+    this.result.addEventListener('click', this.eventResult.bind(this));
 
     for(let i = 0; i < this.lengthData; i++) {
       if (this.data[i].toLowerCase().indexOf(this.keyword) > -1) {
-        if (this.listArray.length < this.listLimit) {
-          this.listArray.push(this.data[i]);
-          this.list = this.createElement(this.resultEl);
-          this.appendResult(this.result, 'afterbegin', this.list, this.data[i]);
-        }
+        if (this.listArray.length >= this.listLimit) return;
+
+        this.insertArray(i);
       }
     }
+  };
+
+  Search.prototype.insertArray = function(index) {
+    this.listArray.push(this.data[index]);
+    this.list = this.createElement(this.resultEl);
+    this.appendResult(this.result, 'afterbegin', this.list, this.data[index]);
   };
 
   Search.prototype.selectResult = function(key) {
     this.wordList = this.result.querySelectorAll('li');
     this.wordLength = this.wordList.length;
     this.wordLastIndex = this.wordLength - 1;
-    if (this.wordLength === this.FIRST_INDEX) {
-      return;
-    }
+
+    if (this.wordLength === this.FIRST_INDEX) return;
     this.moveWord(key === 40 ? 'down' : 'up');
+  };
+
+  Search.prototype.eventResult = function() {
+    this.target = event.target;
+    if (this.target.tagName !== 'LI') return;
+    this.submit(this.target.innerText);
   };
 
   Search.prototype.moveWord = function(direction) {
     if (!this.result.querySelector('.' + this.toggleClass)) {
-      return this.wordList[this.FIRST_INDEX].classList.add(this.toggleClass);
+      return this.moveInit();
     }
 
-    for (let i = 0; i < this.wordLength; i++) {
-      this.wordList[i].classList.remove(this.toggleClass);
-    }
+    this.removeMove();
 
     if (direction === 'down') {
       this.wordIndex = this.wordIndex === this.wordLastIndex ? this.FIRST_INDEX : ++this.wordIndex;
@@ -146,15 +159,34 @@ const search = (function() {
     }
 
     this.wordList[this.wordIndex].classList.add(this.toggleClass);
+    this.changeValue(this.wordList[this.wordIndex].innerText);
   };
 
-  Search.prototype.submit = function() {
+  Search.prototype.moveInit = function() {
+    if (!this.result.querySelector('.' + this.toggleClass)) {
+      this.changeValue(this.wordList[this.FIRST_INDEX].innerText);
+      return this.wordList[this.FIRST_INDEX].classList.add(this.toggleClass);
+    }
+  };
+
+  Search.prototype.removeMove = function() {
+    for (let i = 0; i < this.wordLength; i++) {
+      this.wordList[i].classList.remove(this.toggleClass);
+    }
+  };
+
+  Search.prototype.changeValue = function(value) {
+    this.input.value = value;
+  };
+
+  Search.prototype.submit = function(value) {
     event.preventDefault();
     if (this.result.querySelector('.' + this.toggleClass)) {
       return console.log(this.result.querySelector('.' + this.toggleClass).innerText);
     }
-
-    console.log(this.input.value);
+    this.inputValue = value || this.input.value;
+    this.changeValue(this.inputValue);
+    console.log(this.inputValue);
   };
 
   return {
