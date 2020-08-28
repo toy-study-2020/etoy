a869add0d131f0daee121146002a12908c06aefc
tnwjd2050 <tnwjd2050@naver.com>
2020-08-28 23:16:43 +0900

    기능 구현 완료

--- 박수정/interest/src/index.js
+++
@@ -1,3 +0,0 @@
-const root = document.querySelector('#app');
-
-// import export 모듈패턴을 이용하여 TODO
---
+++ 박수정/interest/src/scripts/index.js
@@ -0,0 +1,49 @@
+const root = document.querySelector('#app');
+const container = document.querySelector('.input__container');
+const listWrap = container.querySelector('ul');
+const input = container.querySelector('input');
+
+function keycheck(evt) {
+    return evt.which ? evt.which : event.keyCode;
+}
+
+function initializationInput() {
+    input.value = '';
+    input.focus();
+}
+
+function removeList(evt) {
+    let selector = evt.target.parentElement;
+
+    if (evt.target.tagName === 'INPUT') {
+        listWrap.lastElementChild.remove();
+    }
+}
+
+function addList(val) {
+    initializationInput();
+    listWrap.insertAdjacentHTML('beforeend', `<li>${val}<button type="button" class="deleteBtn">✖</button></li>`)
+}
+
+input.addEventListener('keyup', function(e) {
+    const value = this.value.replace(/,/gi, "");
+
+    switch (keycheck(e)) {
+        case 188:
+            if (!value) return initializationInput();
+
+            addList(value);
+            break;
+
+        case 46:
+            removeList(e);
+            break;
+
+        default:
+            break;
+    }
+})
+
+document.addEventListener('click', e => {
+    if (e.target.classList.contains('deleteBtn')) removeList(e);
+});
