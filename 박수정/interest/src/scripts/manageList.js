588946b2174149de9b14f3fbb40bc79e4814904b
tnwjd2050 <tnwjd2050@naver.com>
2020-08-29 19:04:17 +0900

    관심사 배열추가/ 중복내용 등록 못하도록 방어

--- 박수정/interest/src/scripts/manageList.js
+++ 박수정/interest/src/scripts/manageList.js
@@ -1,18 +1,34 @@
 import { listWrap, input, initializationInput } from "./utils.js";
 
+let interestList = [];
+
 function removeList(evt) {
     if (!listWrap.lastElementChild) return;
 
-    let selector = evt.target.tagName === 'INPUT'
-     ? listWrap.lastElementChild : evt.target.parentElement;
+    let selector = listWrap.lastElementChild;
+
+    if (evt.target.tagName === 'INPUT') {
+        interestList.pop();
+    } else {
+        selector = evt.target.parentElement;
+        interestList = interestList.filter(item => {
+            return item !== selector.querySelector('span').innerText;
+        })
+    }
 
     selector.remove();
     initializationInput(input);
 }
 
 function addList(val) {
+    if (interestList.includes(val)) {
+        alert('이미 등록되어 있는 관심사입니다.');
+    } else {
+        listWrap.insertAdjacentHTML('beforeend', `<li><span>${val}</span><button type="button" class="deleteBtn">✖</button></li>`)
+        interestList.push(val);
+    }
+
     initializationInput(input);
-    listWrap.insertAdjacentHTML('beforeend', `<li>${val}<button type="button" class="deleteBtn">✖</button></li>`)
 }
 
 export { removeList, addList };
